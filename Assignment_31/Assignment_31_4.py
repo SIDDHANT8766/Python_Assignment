008e91fe1463e3debd4efc17e7901791aa72bcac
Siddhant Gadkari <gadkari.siddhant.vivek@gmail.com>
2026-02-19 00:19:00 +0530

    Initial Upload

---
+++ Assignment_31/Assignment_31_4.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import shutil
+
+def CheckFile(Directory,NewDirectory,Extention):
+
+    Ret = os.path.exists(Directory)
+    if(Ret == False):
+        print("Folder is not exist")
+        return 
+
+    Ret = os.path.isdir(Directory)
+    if(Ret == False):
+        print("It is not a folder/Directory")
+        return
+    
+    Ret = os.mkdir(NewDirectory)
+
+    if(Ret == False):
+        print("unable to create folder") 
+
+    
+    
+    for FolderName, SubFolderName, FileName in os.walk(Directory):
+        for file in FileName:
+            if(file.endswith(Extention) == True):
+            
+                src_path = os.path.join(FolderName,file)
+
+                shutil.copy(src_path,NewDirectory)
+
+        print("File copied successfully")
+
+
+def main():
+
+    CheckFile(sys.argv[1],sys.argv[2],sys.argv[3])
+
+if __name__ == "__main__":
+    main()
